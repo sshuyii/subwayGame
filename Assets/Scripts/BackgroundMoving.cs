882f893aad2b78c5adbca2e03724986221aee484
shuyi <mycalendula@gmail.com>
2019-10-24 00:20:38 -0400

    milestone except animation.

--- Assets/Scripts/BackgroundMoving.cs
+++ Assets/Scripts/BackgroundMoving.cs
@@ -6,23 +6,25 @@ public class BackgroundMoving : MonoBehaviour
 {
     public float speed;
 
-    public float startPoint;
+    public float endPoint;
+
+    private float startPoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPoint = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x < startPoint)
+        if(transform.position.x < endPoint)
         {
             transform.position += new Vector3(speed, 0, 0);
         }
         else
         {
-            transform.position -= new Vector3(startPoint, 0, 0);
+            transform.position -= new Vector3(endPoint - startPoint, 0, 0);
         }
         
     }
