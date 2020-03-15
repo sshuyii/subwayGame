8ef8e4e4474b476a85aba9b806940df71b091dd3
shuyi <mycalendula@gmail.com>
2020-03-15 03:32:35 -0400

    地铁转圈，时间正确，停止enabled

--- Assets/Scripts/PathFollower.cs
+++ Assets/Scripts/PathFollower.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PathFollower : MonoBehaviour
@@ -10,12 +11,18 @@ public class PathFollower : MonoBehaviour
 
     // Walk speed that can be set in Inspector
     [SerializeField]
-    private float moveSpeed = 2f;
+    private float distance = 2f;
 
     // Index of current waypoint from which Enemy walks
     // to the next one
-    private int waypointIndex = 0;
+    public int waypointIndex = 0;
     private RectTransform myRT;
+    private SubwayMovement SubwayMovement;
+
+    public List<int> stationNumList;
+    private float moveSpeed;
+    private bool trainMove;
+    private Vector3 currentEulerAngles;
 
     // Use this for initialization
     private void Start () {
@@ -23,6 +30,12 @@ public class PathFollower : MonoBehaviour
         // Set position of Enemy as position of the first waypoint
         myRT = GetComponent<RectTransform>();
         myRT.anchoredPosition= waypoints[waypointIndex].anchoredPosition;
+        
+        SubwayMovement = GameObject.Find("---StationController").GetComponent<SubwayMovement>();
+
+        moveSpeed = distance / SubwayMovement.moveTime;
+        currentEulerAngles = myRT.eulerAngles;
+
     }
 	
     // Update is called once per frame
@@ -32,14 +45,37 @@ public class PathFollower : MonoBehaviour
         Move();
     }
 
+    private float timer;
+
+    private bool timeToGo;
     // Method that actually make Enemy walk
     private void Move()
     {
 //        print("isWorking");
         // If Enemy didn't reach last waypoint it can move
         // If enemy reached last waypoint then it stops
-        if (waypointIndex <= waypoints.Length - 1)
+
+        
+
+        if (!trainMove)
         {
+            if (SubwayMovement.isMoving)
+            {
+                trainMove = true;
+                moveSpeed = distance / SubwayMovement.moveTime;
+            }
+            else
+            {
+                moveSpeed = 0;
+                myRT.eulerAngles = currentEulerAngles;
+            }
+        }
+
+
+        if (waypointIndex <= waypoints.Length - 1 && trainMove)
+        {
+            currentEulerAngles = myRT.eulerAngles;
+
             //print(waypointIndex + "<1");
             // Move Enemy from current waypoint to the next one
             // using MoveTowards method
@@ -53,19 +89,28 @@ public class PathFollower : MonoBehaviour
             myRT.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
                 
                 
-                
             // If Enemy reaches position of waypoint he walked towards
             // then waypointIndex is increased by 1
             // and Enemy starts to walk to the next waypoint
             if (myRT.anchoredPosition == waypoints[waypointIndex].anchoredPosition)
             {
                 waypointIndex += 1;
+                
+                for (int i = 0; i < stationNumList.Count; i++)
+                {
+                    if (waypointIndex == stationNumList[i])
+                    {
+                        trainMove = false;
+                        break;
+                    }
+                }
             }
         }
-        else
+        else if(trainMove)
         {
             waypointIndex = 0;
-            print("=00000");
+            trainMove = false;
+//            print("=00000");
         }
     }
 }
